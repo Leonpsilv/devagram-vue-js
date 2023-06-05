a85e6fb18a71687485bd8647562ae822157c66f2
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-06-05 16:12:52 -0400

    login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,45 @@
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import {defineComponent} from 'vue';
     export default defineComponent({
-        
-    })
+        data(){
+            return {
+                login: '',
+                password: '',
+                loading: false,
+                error : ''
+            }
+        },
+        methods: {
+            doLogin(){
+                if(!this.login && !this.password){
+                    this.error = 'Favor preencher o formulário';
+                    return;
+                }
+                alert('Login efetuado: ' + this.login + ", " + this.password);
+            }
+        }
+    });
 </script>
+
 <template>
-    <h1>Olá mundo! Vue.js aqui</h1>
+    <h1>Bem vindo ao Devagram!</h1>
+    <div>
+        <img src="../assets/images/logo.svg" alt="Logo Devagram"/>
+        <form>
+            <p v-if="error">{{error}}</p>
+            <div>
+                <img src="../assets/images/envelope.svg" alt="Insira o login"/>
+                <input type="text" v-model="login" />
+            </div>
+            <div>
+                <img src="../assets/images/key.svg" alt="Insira a senha"/>
+                <input type="password" v-model="password" />
+            </div>
+            <button @click.enter.prevent="doLogin">Login</button>
+            <div>
+                <p>Não possui uma conta?</p>
+                <a>Faça seu cadastro agora!</a>
+            </div>
+        </form>
+    </div>
 </template>
