69d23078d8f423110fd5c2918970a0bdfcedf185
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-06-05 21:56:13 -0400

    login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,59 @@
 <script lang="ts">
-    import {defineComponent} from 'vue';
-    export default defineComponent({
-        data(){
-            return {
-                login: '',
-                password: '',
-                loading: false,
-                error : ''
-            }
-        },
-        methods: {
-            doLogin(){
-                if(!this.login && !this.password){
-                    this.error = 'Favor preencher o formulário';
-                    return;
-                }
-                alert('Login efetuado: ' + this.login + ", " + this.password);
-            }
+import { defineComponent } from 'vue';
+import PublicInput from '@/components/PublicInput.vue';
+import loginIcon from '../assets/images/envelope.svg';
+import passwordIcon from '../assets/images/key.svg';
+
+export default defineComponent({
+    setup(){
+        return{
+            loginIcon,
+            passwordIcon
         }
-    });
+    },
+    data() {
+        return {
+            login: '',
+            password: '',
+            loading: false,
+            error: ''
+        }
+    },
+    methods: {
+        doLogin() {
+            if (!this.login && !this.password) {
+                this.error = 'Favor preencher o formulário';
+                return;
+            }
+            alert('Login efetuado: ' + this.login + ", " + this.password);
+        },
+        setLogin(v: any){
+            this.login = v
+        },
+        setPassword(v: any){
+            this.password = v
+        }
+    }
+});
 </script>
 
 <template>
-    <h1>Bem vindo ao Devagram!</h1>
-    <div>
-        <img src="../assets/images/logo.svg" alt="Logo Devagram"/>
+    <div class="public-container">
+        <img src="../assets/images/logo.svg" alt="Logo Devagram" class="logo" />
         <form>
-            <p v-if="error">{{error}}</p>
-            <div>
-                <img src="../assets/images/envelope.svg" alt="Insira o login"/>
-                <input type="text" v-model="login" />
-            </div>
-            <div>
-                <img src="../assets/images/key.svg" alt="Insira a senha"/>
-                <input type="password" v-model="password" />
-            </div>
+            <p v-if="error" class="error">{{ error }}</p>
+            <PublicInput :icon="loginIcon" alt="Insira o login" type="text" placeholder="Email"
+                :modelValue="login" @setInput="setLogin"/>
+            <PublicInput :icon="passwordIcon" alt="Insira a senha" type="password" placeholder="Senha"
+                :modelValue="password" @setInput="setPassword"/>
+
             <button @click.enter.prevent="doLogin">Login</button>
-            <div>
+            <div class="link">
                 <p>Não possui uma conta?</p>
                 <a>Faça seu cadastro agora!</a>
             </div>
         </form>
     </div>
 </template>
+
+<style lang="scss" src="@/assets/styles/publicPage.scss" />
