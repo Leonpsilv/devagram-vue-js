5b64064251ce00871f35f2461ccb0a1803e20a6f
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-06-08 19:46:20 -0400

    footer in mobile

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import Navigation from './Navigation.vue'
+    
+    export default defineComponent({
+        components: { Navigation }
+    });
+</script>
+<template>
+    <footer class="footer-container">
+        <Navigation />
+    </footer>
+</template>
+
+<style lang="scss" src="@/assets/styles/footer.scss" />
